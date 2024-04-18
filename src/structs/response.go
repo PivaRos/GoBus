a83d2da1a6c01294b8004cfda89132a3f276560e
Daniel Gurbin <daniel.g@msapps.mobi>
2024-04-18 22:25:18 +0300

    remove xmls

--- src/structs/response.go
+++ src/structs/response.go
@@ -6,76 +6,76 @@ import (
 )
 
 type ResponseData struct {
-	XMLName xml.Name `json:"-" xml:"Siri"`
-	Siri    Siri     `json:"Siri" xml:"Siri"`
+	XMLName xml.Name `json:"-"`
+	Siri    Siri     `json:"Siri"`
 }
 
 type Siri struct {
-	ServiceDelivery ServiceDelivery `json:"ServiceDelivery" xml:"ServiceDelivery"`
+	ServiceDelivery ServiceDelivery `json:"ServiceDelivery"`
 }
 
 type ServiceDelivery struct {
-	ResponseTimestamp         time.Time                `json:"ResponseTimestamp" xml:"ResponseTimestamp"`
-	ProducerRef               string                   `json:"ProducerRef" xml:"ProducerRef"`
-	ResponseMessageIdentifier string                   `json:"ResponseMessageIdentifier" xml:"ResponseMessageIdentifier"`
-	RequestMessageRef         string                   `json:"RequestMessageRef" xml:"RequestMessageRef"`
-	Status                    string                   `json:"Status" xml:"Status"`
-	StopMonitoringDelivery    []StopMonitoringDelivery `json:"StopMonitoringDelivery" xml:"StopMonitoringDelivery"`
+	ResponseTimestamp         time.Time                `json:"ResponseTimestamp"`
+	ProducerRef               string                   `json:"ProducerRef"`
+	ResponseMessageIdentifier string                   `json:"ResponseMessageIdentifier"`
+	RequestMessageRef         string                   `json:"RequestMessageRef"`
+	Status                    string                   `json:"Status"`
+	StopMonitoringDelivery    []StopMonitoringDelivery `json:"StopMonitoringDelivery"`
 }
 
 type StopMonitoringDelivery struct {
-	Version            string                `json:"-" xml:"version,attr"`
-	ResponseTimestamp  time.Time             `json:"ResponseTimestamp" xml:"ResponseTimestamp"`
-	Status             string                `json:"Status" xml:"Status"`
-	MonitoredStopVisit *[]MonitoredStopVisit `json:"MonitoredStopVisit,omitempty" xml:"MonitoredStopVisit,omitempty"`
-	ErrorCondition     *ErrorCondition       `json:"ErrorCondition,omitempty" xml:"ErrorCondition,omitempty"`
+	Version            string                `json:"-"attr"`
+	ResponseTimestamp  time.Time             `json:"ResponseTimestamp"`
+	Status             string                `json:"Status"`
+	MonitoredStopVisit *[]MonitoredStopVisit `json:"MonitoredStopVisit,omitempty"`
+	ErrorCondition     *ErrorCondition       `json:"ErrorCondition,omitempty"`
 }
 
 type MonitoredStopVisit struct {
-	RecordedAtTime          time.Time               `json:"RecordedAtTime" xml:"RecordedAtTime"`
-	ItemIdentifier          string                  `json:"ItemIdentifier" xml:"ItemIdentifier"`
-	MonitoringRef           string                  `json:"MonitoringRef" xml:"MonitoringRef"`
-	MonitoredVehicleJourney MonitoredVehicleJourney `json:"MonitoredVehicleJourney" xml:"MonitoredVehicleJourney"`
+	RecordedAtTime          time.Time               `json:"RecordedAtTime"`
+	ItemIdentifier          string                  `json:"ItemIdentifier"`
+	MonitoringRef           string                  `json:"MonitoringRef"`
+	MonitoredVehicleJourney MonitoredVehicleJourney `json:"MonitoredVehicleJourney"`
 }
 
 type MonitoredVehicleJourney struct {
-	LineRef                  string                  `json:"LineRef" xml:"LineRef"`
-	DirectionRef             string                  `json:"DirectionRef" xml:"DirectionRef"`
-	FramedVehicleJourneyRef  FramedVehicleJourneyRef `json:"FramedVehicleJourneyRef" xml:"FramedVehicleJourneyRef"`
-	PublishedLineName        string                  `json:"PublishedLineName" xml:"PublishedLineName"`
-	OperatorRef              string                  `json:"OperatorRef" xml:"OperatorRef"`
-	DestinationRef           string                  `json:"DestinationRef" xml:"DestinationRef"`
-	OriginAimedDepartureTime time.Time               `json:"OriginAimedDepartureTime" xml:"OriginAimedDepartureTime"`
-	ConfidenceLevel          string                  `json:"ConfidenceLevel" xml:"ConfidenceLevel"`
-	VehicleLocation          VehicleLocation         `json:"VehicleLocation" xml:"VehicleLocation"`
-	Bearing                  string                  `json:"Bearing" xml:"Bearing"`
-	Velocity                 string                  `json:"Velocity" xml:"Velocity"`
-	VehicleRef               string                  `json:"VehicleRef" xml:"VehicleRef"`
-	MonitoredCall            MonitoredCall           `json:"MonitoredCall" xml:"MonitoredCall"`
+	LineRef                  string                  `json:"LineRef"`
+	DirectionRef             string                  `json:"DirectionRef"`
+	FramedVehicleJourneyRef  FramedVehicleJourneyRef `json:"FramedVehicleJourneyRef"`
+	PublishedLineName        string                  `json:"PublishedLineName"`
+	OperatorRef              string                  `json:"OperatorRef"`
+	DestinationRef           string                  `json:"DestinationRef"`
+	OriginAimedDepartureTime time.Time               `json:"OriginAimedDepartureTime"`
+	ConfidenceLevel          string                  `json:"ConfidenceLevel"`
+	VehicleLocation          VehicleLocation         `json:"VehicleLocation"`
+	Bearing                  string                  `json:"Bearing"`
+	Velocity                 string                  `json:"Velocity"`
+	VehicleRef               string                  `json:"VehicleRef"`
+	MonitoredCall            MonitoredCall           `json:"MonitoredCall"`
 }
 
 type FramedVehicleJourneyRef struct {
-	DataFrameRef           string `json:"DataFrameRef" xml:"DataFrameRef"`
-	DatedVehicleJourneyRef string `json:"DatedVehicleJourneyRef" xml:"DatedVehicleJourneyRef"`
+	DataFrameRef           string `json:"DataFrameRef"`
+	DatedVehicleJourneyRef string `json:"DatedVehicleJourneyRef"`
 }
 
 type VehicleLocation struct {
-	Longitude string `json:"Longitude" xml:"Longitude"`
-	Latitude  string `json:"Latitude" xml:"Latitude"`
+	Longitude string `json:"Longitude"`
+	Latitude  string `json:"Latitude"`
 }
 
 type MonitoredCall struct {
-	StopPointRef        string    `json:"StopPointRef" xml:"StopPointRef"`
-	Order               string    `json:"Order" xml:"Order"`
-	ExpectedArrivalTime time.Time `json:"ExpectedArrivalTime" xml:"ExpectedArrivalTime"`
-	DistanceFromStop    string    `json:"DistanceFromStop" xml:"DistanceFromStop"`
+	StopPointRef        string    `json:"StopPointRef"`
+	Order               string    `json:"Order"`
+	ExpectedArrivalTime time.Time `json:"ExpectedArrivalTime"`
+	DistanceFromStop    string    `json:"DistanceFromStop"`
 }
 
 type ErrorCondition struct {
-	OtherError  *OtherError `json:"OtherError,omitempty" xml:"OtherError,omitempty"`
-	Description string      `json:"Description" xml:"Description"`
+	OtherError  *OtherError `json:"OtherError,omitempty"omitempty"`
+	Description string      `json:"Description"`
 }
 
 type OtherError struct {
-	ErrorText string `json:"ErrorText" xml:"ErrorText"`
+	ErrorText string `json:"ErrorText"`
 }
