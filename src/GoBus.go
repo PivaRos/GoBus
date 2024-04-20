cd07a17d9cfca7f2ad301f5fae898da2770b95bd
Daniel Gurbin <daniel.g@msapps.mobi>
2024-04-21 00:31:31 +0300

    bug:using same request resulting the same response

--- src/GoBus.go
+++ src/GoBus.go
@@ -63,39 +63,46 @@ func InitGoBus(options GoBusOptions) (*GoBus, error) {
 func (bus *GoBus) MonitoringRef(MonitoringId string) (*structs.ResponseData, error) {
 	result, redisErr := bus.rdb.Get(context.TODO(), MonitoringId).Result()
 	if redisErr == redis.Nil {
-		request := bus.instance
+		request := bus.instance.Clone(context.Background())
 		params := request.URL.Query()
 		params.Add("MonitoringRef", MonitoringId)
 		request.URL.RawQuery = params.Encode()
 
-		r, httpErr := bus.client.Do(request.Request)
+		r, httpErr := bus.client.Do(request)
 		if httpErr != nil {
 			return nil, httpErr
 		}
-		var data structs.ResponseData
-		err := json.NewDecoder(r.Body).Decode(&data)
+		Data := structs.ResponseData{}
+		err := json.NewDecoder(r.Body).Decode(&Data)
 		if err != nil {
 			return nil, err
 		}
-		if data.Siri.ServiceDelivery.Status == "false" {
-			return nil, errors.New(data.Siri.ServiceDelivery.ErrorCondition.Description)
-		}
-		if data.Siri.ServiceDelivery.StopMonitoringDelivery[0].Status == "false" {
-			return nil, errors.New(data.Siri.ServiceDelivery.StopMonitoringDelivery[0].ErrorCondition.Description)
-		}
-		byteArray, marshalErr := json.Marshal(data)
+		byteArray, marshalErr := json.Marshal(Data)
 		if marshalErr != nil {
 			return nil, marshalErr
 		}
 		bus.rdb.Set(context.TODO(), MonitoringId, byteArray, bus.options.StaleTime)
-		return &data, nil
+		return handleResponse(&Data)
 	} else if redisErr != nil {
 		return nil, redisErr
+	} else {
+		//does have entry in redis
+		Data := structs.ResponseData{}
+		marshalErr := json.Unmarshal([]byte(result), &Data)
+		if marshalErr != nil {
+			return nil, marshalErr
+		}
+		return handleResponse(&Data)
 	}
-	Data := &structs.ResponseData{}
-	marshalErr := json.Unmarshal([]byte(result), Data)
-	if marshalErr != nil {
-		return nil, marshalErr
+}
+
+func handleResponse(data *structs.ResponseData) (*structs.ResponseData, error) {
+	if data.Siri.ServiceDelivery.Status == "false" {
+		return nil, errors.New(data.Siri.ServiceDelivery.ErrorCondition.Description)
 	}
-	return Data, nil
+	if data.Siri.ServiceDelivery.StopMonitoringDelivery[0].Status == "false" {
+		return nil, errors.New(data.Siri.ServiceDelivery.StopMonitoringDelivery[0].ErrorCondition.Description)
+	}
+	//if response does not consist of errors return the response struct
+	return data, nil
 }
